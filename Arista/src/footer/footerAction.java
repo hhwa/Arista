5e8c729c07ef2fb484e23704fb0b0d55212931b0
보영 <zzzzz7z@naver.com>
2018-05-29 13:49:18 +0900

    footer

---
+++ Arista/src/footer/footerAction.java
@@ -0,0 +1,13 @@
+package footer;
+
+import java.io.IOException;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class footerAction extends ActionSupport{
+	
+	public String layout() throws Exception{
+		return SUCCESS;
+	}
+
+}
