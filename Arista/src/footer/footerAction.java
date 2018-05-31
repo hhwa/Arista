7e8c8785e46fba658c28961fd2d08434fc2d9236
HH\hyunh <hyunhwa91@naver.com>
2018-05-31 09:28:35 +0900

    no message

--- Arista/src/footer/footerAction.java
+++ Arista/src/footer/footerAction.java
@@ -1,17 +1,5 @@
 package footer;
 
-<<<<<<< HEAD
-import java.io.IOException;
-
-import com.opensymphony.xwork2.ActionSupport;
-
-public class footerAction extends ActionSupport{
-	
-	public String layout() throws Exception{
-		return SUCCESS;
-	}
-
-=======
 import com.opensymphony.xwork2.ActionSupport;
 
 public class footerAction extends ActionSupport{
@@ -19,5 +7,4 @@ public class footerAction extends ActionSupport{
 	public String layout() throws Exception{
 		return SUCCESS;
 	}
->>>>>>> hh
 }
