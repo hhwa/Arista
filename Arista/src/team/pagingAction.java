07900a8f0afc83f6280eefcabf05eadeeaf825ce
kw <3821123@naver.com>
2018-05-31 15:14:25 +0900

    no message

--- Arista/src/team/pagingAction.java
+++ Arista/src/team/pagingAction.java
@@ -49,10 +49,10 @@ public class pagingAction {
 		if(currentPage > blockPage)
 		{
 			if(isSearch != "")
-				pagingHtml.append("<a href=listAction.action?currentPage=" + (startPage - 1) + "&searchKeyword="+isSearch+"&searchNum="+searchNum+">");
+				pagingHtml.append("<a href=teamlistAction.action?currentPage=" + (startPage - 1) + "&searchKeyword="+isSearch+"&searchNum="+searchNum+">");
 			else
-				pagingHtml.append("<a href=listAction.action?currentPage=" + (startPage - 1) + ">");
-			pagingHtml.append("?��?��");
+				pagingHtml.append("<a href=teamlistAction.action?currentPage=" + (startPage - 1) + ">");
+			pagingHtml.append("이전");
 			pagingHtml.append("</a>");
 		}
 		
@@ -72,7 +72,7 @@ public class pagingAction {
 			}
 			else
 			{
-				pagingHtml.append("&nbsp;<a href='listAction.action?currentPage=");
+				pagingHtml.append("&nbsp;<a href='teamlistAction.action?currentPage=");
 				pagingHtml.append(i);
 				if(isSearch != "")
 					pagingHtml.append("&searchKeyword="+isSearch);
@@ -88,12 +88,12 @@ public class pagingAction {
 		
 		if(totalPage - startPage >= blockPage)
 		{
-			pagingHtml.append("&nbsp;<a href='listAction.action?currentPage=");
+			pagingHtml.append("&nbsp;<a href='teamlistAction.action?currentPage=");
 			pagingHtml.append((endPage+1));
 			if(isSearch != "")
 				pagingHtml.append("&searchKeyword="+isSearch);
 			pagingHtml.append("'>");
-			pagingHtml.append("?��?��");
+			pagingHtml.append("다음");
 			pagingHtml.append("</a>");
 		}
 	}
